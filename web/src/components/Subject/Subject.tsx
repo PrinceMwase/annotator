586e3b984453cebb3e509e97dac27a1a1a272852
PrinceMwase <princefranklinemwase@gmail.com>
2022-07-30 19:08:10 +0200

    break tokens by adjusting slider

--- web/src/components/Subject/Subject.tsx
+++ web/src/components/Subject/Subject.tsx
@@ -27,29 +27,51 @@ const UPOS = {
   X: 'other',
 }
 
-
 interface Props {
   subject: { sentence: String; Token: Token[] }
 }
 
 const Subject = ({ subject }: Props) => {
 
+  const [breaks, setbreaks] = useState([])
 
+
+  // store parts of speech
   const [tags, setTags] = useState([])
 
+  // The current Word Token
   const [thisToken, setToken] = React.useState()
 
+  // The current part of speech of the token
   const [pos, setPos] = useState()
+
+  // store the subject
   const [sentence, setSentence] = React.useState(subject.sentence)
+
+  // store the right part of the sentence whene sliced
   const [rightword, setRight] = React.useState('')
+
+  // store the left part of the sentence when sliced
   const [leftword, setLeft] = React.useState('')
+
+  // store the word
   const [word, setWord] = React.useState('')
+
+  const [wordLength, setwordLength] = useState(0)
 
   /**
    * handle clicked token
    * @param event
    */
+
+  const limiterClicked = (event: { target: { value: any } }) => {
+    const limit = event.target.value
+
+    setbreaks([word.slice(0, limit), word.slice(limit)])
+  }
+
   const tokenClicked = (event: { target: any }) => {
+    setbreaks([])
     setPos(event.target.dataset.pos)
     const raw_token = event.target.innerText
     event.target.classList.add('text-lg')
@@ -62,6 +84,7 @@ const Subject = ({ subject }: Props) => {
     const left_words = sentence.slice(0, word_index)
 
     const word_length = raw_token.length
+    setwordLength(word_length)
 
     let word = right_words.slice(0, word_length)
     right_words = right_words.slice(word_length)
@@ -70,19 +93,19 @@ const Subject = ({ subject }: Props) => {
     setRight(right_words)
     setWord(word)
 
+    // make the selected token standout
     if (thisToken) {
       if (thisToken !== event.target) {
         thisToken.classList.remove('text-lg')
       }
     }
 
-
+    // set the parts of speech tags
     if (tags.length === 0) {
-      let getTags=[];
+      let getTags = []
       Object.entries(UPOS).forEach(([key, value]) => {
         getTags.push(key)
-      });
-      console.log(getTags);
+      })
 
       setTags(getTags)
     }
@@ -116,13 +139,50 @@ const Subject = ({ subject }: Props) => {
               </div>
             ))}
           </div>
-          <ul className="list-none">
-            {
-              tags.length > 0 ? tags.map( x => <li key={x}> {x} </li>)
-              : (<li>empty</li>)
 
-            }
+          {/* Break Word */}
+          <input
+            type="range"
+            min="0"
+            max={wordLength + 1}
+            className="range range-accent range-xs"
+            step="1"
+            onChange={limiterClicked}
+          />
+          {/* <div className="w-full flex justify-between text-xs px-2">
 
+          </div> */}
+
+          {/* Show Broken words */}
+          <div className="flex w-full tooltip tooltip-open tooltip-bottom  " data-tip="use the slider to slice token">
+
+            <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
+              {breaks.length > 0 ? breaks[0] : "nothing to show"}
+
+            </div>
+            <div className="divider divider-horizontal">and</div>
+            <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
+            {breaks.length > 0 ? breaks[1] : "nothing to show"}
+            </div>
+          </div>
+
+          {/* Universal parts of speech */}
+          <ul className="list-none py-10">
+            {tags.length > 0 ? (
+              tags.map((x) => (
+                <li
+                  className={
+                    pos === x ? 'text-purple-700' : 'dark:text-yellow-400'
+                  }
+                  key={x}
+                >
+                  {' '}
+                  {x}{' '}
+                </li>
+              ))
+            ) : (
+              <li>empty</li>
+            )}
           </ul>
         </div>
       </div>
