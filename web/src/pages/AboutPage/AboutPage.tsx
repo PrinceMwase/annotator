1d7150ee668bbbeaf2dacde6e6fa9a9b69bb3a4b
Prince Mwase <princefranklinemwase@gmail.com>
2022-09-07 15:07:15 +0200

    fixed links not working

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -6,21 +6,21 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
 
-      <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card bg-base-100 shadow-xl">
         <div class="card-body">
           <h2 class="card-title">Annotator</h2>
           <p>The goal of this app is to</p>
           <ul class="steps steps-vertical">
-            <li class="step step-primary">segment words</li>
-            <li class="step step-primary">assign parts of speech as tokenization to stemmed words</li>
-            <li class="step step-primary">submit</li>
+            <li class="step step-primary">segment</li>
+            <li class="step step-primary">tokenize</li>
+            <li class="step step-primary">submit sentence</li>
           </ul>
           <p>
-            the result data will be used to train parts of speech data in accordance with <Link to="https://universaldependencies.org/guidelines.html">Universal Dependancies</Link>
+            the result data will be used to train parts of speech data in accordance with <a class="link link-accent" href="https://universaldependencies.org/guidelines.html">Universal Dependancies</a> to create a model of a parts of speech tagger
           </p>
 
           <p>
-            Tokenization and segmentation guidelines can be found at <Link to="https://universaldependencies.org/u/overview/tokenization.html">Universal Dependancies</Link>
+            Tokenization and segmentation guidelines can be found  <a class="link link-accent" href="https://universaldependencies.org/u/overview/tokenization.html"> here </a>
           </p>
           <div class="card-actions justify-end">
 
